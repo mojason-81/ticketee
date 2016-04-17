3a6fb98ba13b33df2182ea5d726f4abf462dd91d
Jason Force <jason@mojason.com>
2016-04-17 14:16:06 -0500

    Link tickets and users upon ticket creation

--- spec/features/deleting_tickets_spec.rb
+++ spec/features/deleting_tickets_spec.rb
@@ -1,8 +1,11 @@
 require "rails_helper"
 
 RSpec.feature "Users can delete tickets" do
+  let(:author)  { FactoryGirl.create(:user) }
   let(:project) { FactoryGirl.create(:project) }
-  let(:ticket)  { FactoryGirl.create(:ticket, project: project) }
+  let(:ticket) do
+    FactoryGirl.create(:ticket, project: project, author: author)
+  end
 
   before do
     visit project_ticket_path(project, ticket)
@@ -15,4 +18,3 @@ RSpec.feature "Users can delete tickets" do
     expect(page.current_url).to eq project_url(project)
   end
 end
-
